65cc2b33f78d733d909f8e2c1cbaa1b6b8c30b0d
gavit <gavithrani.geethma99@gmail.com>
2022-12-17 23:21:49 +0530

    a

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gavi',
+      title: 'Auto Car Service',
       theme: ThemeData(
         // This is the theme of your application.
         //

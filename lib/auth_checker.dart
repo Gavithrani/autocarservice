4be1aca389db64b77d94d5345d8df069532d9c88
dilankas2001 <dilankasandeepa1214@gmail.com>
2022-12-18 10:39:20 +0530

    edit1

---
+++ lib/auth_checker.dart
@@ -0,0 +1,37 @@
+import 'package:autocarservice/providers/auth_provider.dart';
+import 'package:autocarservice/home_screen.dart';
+import 'package:autocarservice/login_screen.dart';
+import 'package:autocarservice/wapper.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:autocarservice/spalashscreen.dart';
+
+class AuthChecker extends ConsumerWidget {
+  const AuthChecker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _authState = ref.watch(authStateProvider);
+
+    return _authState.when(
+        data: (user) {
+          if (user != null) return HomePage();
+          return const LoginScreen();
+        },
+        loading: () => const SplashScreen(),
+        error: (e, trace) => const LoginScreen());
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
